71b77b07408c8d31079befbb51ee8596cc8e78f6
brunosiqueira <bsiqueira@gmail.com>
2015-09-14 16:53:13 +0100

    including correct videos size

--- correctVideoDuration.js
+++ correctVideoDuration.js
@@ -6,21 +6,32 @@ var
 //search for all videos which have 0 or 1 as duration.
 db.video.findAll({where: {$or: [{duration: 0}, {duration: 1}]}}).then(function(videos){
   //find the mp4 files
-  for (var i = 0 ; i < videos.length ; i++) {
-    console.log(".");
-    var video = videos[i];
-    //check if the file exists
-    if (!storage.exists(video)) {
-      //if it doesn't, mark the video as deleted
-    } else {
-      //if it does, check the metadata.
-      storage.getTotaDuration(video, function(duration){
-        console.log("new duration for video: "+duration+" - "+this.id);
-        //include the metadata in the
-        this.duration = Math.ceil(duration);
-        this.save();
-      });
-    }
-  }
+  i = videos.length-1;
+  console.log(".");
+  correct(videos, i);
+
 
 });
+
+function correct(videos, i){
+  if (i == 0){
+    console.log('finalized');
+    return;
+  }
+  var video = videos[i];
+  //check if the file exists
+  if (!storage.exists(video)) {
+    //if it doesn't, mark the video as deleted
+    console.log("video does not exists");
+    correct(videos, i-1);
+  } else {
+    //if it does, check the metadata.
+    storage.getTotaDuration(video, function(duration){
+      console.log("new duration for video: "+duration+" - "+this.id);
+      //include the metadata in the
+      this.duration = Math.ceil(duration);
+      this.save();
+      correct(videos, i-1);
+    });
+  }
+}
