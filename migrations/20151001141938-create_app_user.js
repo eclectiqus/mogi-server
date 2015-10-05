42895164da1e0fbeb4f199c27026b9ec0a476c5c
Leandro Martelli <martelli@epix.com.br>
2015-10-05 16:00:18 -0300

    Added missing USAGE on grants

--- migrations/20151001141938-create_app_user.js
+++ migrations/20151001141938-create_app_user.js
@@ -5,10 +5,10 @@ var copcast_db_pass = require('../lib/db').sequelize.config.password;
 module.exports = {
   up: function (queryInterface, Sequelize, done) {
     queryInterface.sequelize.query("CREATE USER "+copcast_db_user+" WITH password :pass", { replacements: {pass: copcast_db_pass}}).then(function() {
-      queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT,INSERT ON TABLES TO "+copcast_db_user).then(function() {
-        queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO "+copcast_db_user).then(function() {
-          queryInterface.sequelize.query("GRANT SELECT,INSERT ON ALL TABLES IN SCHEMA public TO "+copcast_db_user).then(function() {
-            queryInterface.sequelize.query("GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO "+copcast_db_user).then(function() {
+      queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT,INSERT,UPDATE ON TABLES TO "+copcast_db_user).then(function() {
+        queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE,SELECT ON SEQUENCES TO "+copcast_db_user).then(function() {
+          queryInterface.sequelize.query("GRANT SELECT,INSERT,UPDATE ON ALL TABLES IN SCHEMA public TO "+copcast_db_user).then(function() {
+            queryInterface.sequelize.query("GRANT USAGE,SELECT ON ALL SEQUENCES IN SCHEMA public TO "+copcast_db_user).then(function() {
               done();
             });
           });
@@ -20,9 +20,9 @@ module.exports = {
   down: function (queryInterface, Sequelize, done) {
 
     queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT,INSERT ON TABLES FROM "+copcast_db_user).then(function() {
-      queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT ON SEQUENCES FROM "+copcast_db_user).then(function() {
+      queryInterface.sequelize.query("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE USAGE,SELECT ON SEQUENCES FROM "+copcast_db_user).then(function() {
         queryInterface.sequelize.query("REVOKE SELECT,INSERT ON ALL TABLES IN SCHEMA public FROM "+copcast_db_user).then(function() {
-          queryInterface.sequelize.query("REVOKE SELECT ON ALL SEQUENCES IN SCHEMA public FROM "+copcast_db_user).then(function() {
+          queryInterface.sequelize.query("REVOKE USAGE,SELECT ON ALL SEQUENCES IN SCHEMA public FROM "+copcast_db_user).then(function() {
             queryInterface.sequelize.query("DROP USER "+copcast_db_user).then(function() {
               done();
             });
